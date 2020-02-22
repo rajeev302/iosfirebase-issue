8b2c6775e27220c61cea019adf0239818988cd43
Rajeev Ranjan <rajeev@infrrdad.local>
2020-02-22 09:14:58 +0530

    coroutines added

--- app/src/main/java/com/example/iosfirebaseissue/viewmodel/IssueListViewModel.kt
+++ app/src/main/java/com/example/iosfirebaseissue/viewmodel/IssueListViewModel.kt
@@ -2,6 +2,14 @@ package com.example.iosfirebaseissue.viewmodel
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import kotlin.coroutines.CoroutineContext
 
-class IssueListViewModel(application: Application): AndroidViewModel(application) {
+class IssueListViewModel(application: Application): AndroidViewModel(application), CoroutineScope {
+    private val job = Job()
+    override val coroutineContext: CoroutineContext
+        get() = job + Dispatchers.Main
 }
