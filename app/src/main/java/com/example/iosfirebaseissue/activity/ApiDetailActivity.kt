2d3598015d27dab7cb8f35de80b5706e5193bfb6
Rajeev Ranjan <rajeev@infrrdad.local>
2020-07-15 16:08:17 +0530

    different config fetched from file based on differnt flavors

--- app/src/main/java/com/example/iosfirebaseissue/activity/ApiDetailActivity.kt
+++ app/src/main/java/com/example/iosfirebaseissue/activity/ApiDetailActivity.kt
@@ -2,22 +2,17 @@ package com.example.iosfirebaseissue.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Html
-import android.webkit.WebView
 import android.widget.Button
+import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
-import com.android.volley.Request
-import com.android.volley.Response
-import com.android.volley.toolbox.StringRequest
-import com.android.volley.toolbox.Volley
+import com.example.iosfirebaseissue.BuildConfig
 import com.example.iosfirebaseissue.R
-import com.example.iosfirebaseissue.manager.NetworkManager
 import com.example.iosfirebaseissue.viewmodel.ApiDetailViewModel
 import kotlinx.android.synthetic.main.activity_api_detail.*
 
 class ApiDetailActivity : AppCompatActivity() {
 
-    private lateinit var textView: WebView
+    private lateinit var textView: TextView
     private lateinit var button: Button
     private lateinit var viewModel: ApiDetailViewModel
 
@@ -36,21 +31,23 @@ class ApiDetailActivity : AppCompatActivity() {
 
     private fun setupClickListener(){
         button.setOnClickListener {
-            // Instantiate the RequestQueue.
-            val queue = Volley.newRequestQueue(this)
-            val url = NetworkManager.getBaseUrl()
+            textView.text = "Version code = ${BuildConfig.VERSION_CODE.toString()} \n" +
+                    "version name = ${BuildConfig.VERSION_NAME} \n" + "base url = ${BuildConfig.BASE_URL}\n" +
+                    "analytics api key = ${BuildConfig.ANALYTICS_API_KEY}\n" +
+                    "analytics secret key=${BuildConfig.ANALYTICS_API_SECRET}\n"
 
-// Request a string response from the provided URL.
-            val stringRequest = StringRequest(
-                Request.Method.GET, url,
-                Response.Listener<String> { response ->
-                    // Display the first 500 characters of the response string.
-                    textView.loadData(response, "text/html; charset=utf-8", "UTF-8");
-                },
-                Response.ErrorListener {  })
 
-// Add the request to the RequestQueue.
-            queue.add(stringRequest)
+
+//            val queue = Volley.newRequestQueue(this)
+//            val url = NetworkManager.getBaseUrl()
+//            val stringRequest = StringRequest(
+//                Request.Method.GET, url,
+//                Response.Listener<String> { response ->
+//                    textView.loadData(response, "text/html; charset=utf-8", "UTF-8");
+//                },
+//                Response.ErrorListener {  })
+//            queue.add(stringRequest)
+
 
         }
     }
